48ef6dba339464bbf7054ff5d25484d3ee711c61
Sam Hulick <samh@reelcrafter.com>
2022-08-12 16:10:40 -0500

    Initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <client-only>
+      <Boom>
+        <template #test>
+          This is from app.vue
+        </template>
+      </Boom>
+    </client-only>
+  </div>
+</template>
---
+++ components/Boom.client.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const loading = ref(true);
+
+if (process.client) {
+  setTimeout(() => {
+    loading.value = false;
+  }, 1500);
+}
+</script>
+
+<template>
+  <div>BOOM</div>
+  <div v-show="loading">
+    <slot name="test" />
+  </div>
+</template>
